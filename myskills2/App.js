81c9450e28e4cde10f25f0f59df0044bb670cf0b
thaynarbo <thaynarbo@gmail.com>
2022-01-15 10:21:51 -0300

    start files

---
+++ myskills2/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View,Text} from 'react-native';
+import { Home } from './src/pages/Home';
+
+const App = () => {
+  return (
+    <Home/>
+  )
+}
+
+export default App;
